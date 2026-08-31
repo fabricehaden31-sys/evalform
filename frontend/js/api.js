c6d0ab67827439918000bf1080ecd70f5787c43e
fabricehaden31-sys <fabricehaden31@gmail.com>
2026-08-31 11:44:57 +0000

    Fix API base URL for production deployment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,4 @@
-const API_BASE_URL = window.location.origin.includes(‘:8000’)
-  ? `${window.location.origin}/api`
-  : ‘http://127.0.0.1:8000/api’;
+const API_BASE_URL = `${window.location.origin}/api`;
 
 const AUTH_STORAGE_KEY = ‘evalform_role’;
 const USER_STORAGE_KEY = ‘evalform_username’;
